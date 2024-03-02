5e7cfa9efa61f742cb020089a744d4ee41019b53
makampos <matheus.campos@edu.unifil.br>
2024-03-02 17:43:21 -0300

    Just getting started

---
+++ Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using GrainInterfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+IHostBuilder builder = Host.CreateDefaultBuilder(args)
+    .UseOrleansClient(client =>
+    {
+        client.UseLocalhostClustering();
+    })
+    .ConfigureLogging(logging => logging.AddConsole())
+    .UseConsoleLifetime();
+
+using IHost host = builder.Build();
+await host.StartAsync();
+
+IClusterClient client = host.Services.GetRequiredService<IClusterClient>();
+
+IHello friend = client.GetGrain<IHello>(0);
+
+string response = await friend.SayHello("Hi mate!");
+
+Console.WriteLine($"""
+                   {response}
+
+                   Press any key to exit...
+                   """);
+
+Console.ReadKey();
+
+await host.StopAsync();
